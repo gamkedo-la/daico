b263a04bd1af6aee8de36ddef53b6d4eec839fc8
Christer Kaitila <breakdance@mcfunkypants.com>
2022-07-26 13:07:03 -0700

    smoke changed to sparkles on attack fx

--- js/SimpleParticles.js
+++ js/SimpleParticles.js
@@ -212,7 +212,7 @@ function attack_fx(x,y,facingLeft) {
         let velx = Math.random()*6*dir;
         let vely = Math.random()*4-2;
         let alpha = 0.5;
-        particles.add(x+8*dir,y+8,smokePic,life,rotspd,ang,velx,vely,alpha); // dust
+        particles.add(x+8*dir,y+8,sparklePic,life,rotspd,ang,velx,vely,alpha); // dust
         let turnAround = Math.PI;
         particles.add(x+8*dir,y+8,attackFxPic,200,0,facingLeft?turnAround:0,4*dir,0,0.2); //  a woosh )))
     }
