63a3862aaa7e98e43b0344513e83664dfb22dd42
Adgemon <e.t.khani@gmail.com>
2022-09-01 22:42:01 -0400

    little enemy now chasing angel

--- js/LittleEnemy.js
+++ js/LittleEnemy.js
@@ -8,18 +8,36 @@ function LittleEnemyClass() {
     return this.resetOnTile(TILE_LITTLE_ENEMY);
   } // end of reset
   
-  this.AISeeingPlayer = function() {
-    var toPlayer = angTo(p1.x - this.x, p1.y - this.y);
-    this.xv = Math.cos(toPlayer);
-    this.yv = Math.sin(toPlayer);
-    var attackChance = Math.random();
-    if (attackChance < ATTACK_ODDS) {
-        var newSlash = new clawClass();
-        newSlash.x = this.x + this.xv * ATTACK_RANGE;
-        newSlash.y = this.y + this.yv * ATTACK_RANGE;
-        newSlash.ang = toPlayer;
-        enemyAttackList.push(newSlash);
-    }
+  this.AISeeingAngel = function() {
+    var toAngel = angTo(angel.x - this.x, angel.y - this.y);
+    this.xv = Math.cos(toAngel);
+    this.yv = Math.sin(toAngel);
+   
+  }
+
+  this.AISeeingPlayer  = function() {
+  }
+  this.superMove = this.move;
+  this.move = function() {
+      this.superMove();
+      if (dist(this.x - angel.x, this.y - angel.y) < 3*TILE_W) {
+        var lineBlocked = isWallBetweenPoints(this.x, this.y, angel.x, angel.y);
+        if(lineBlocked) {
+          if (dist(this.x - this.lastSeenPlayerX, this.y - this.lastSeenPlayerY) > 10) {
+            var toLast = angTo(this.lastSeenPlayerX - this.x, this.lastSeenPlayerY - this.y);
+            this.xv = Math.cos(toLast);
+            this.yv = Math.sin(toLast);
+          } else {
+            this.xv = 0;
+            this.yv = 0;
+          }
+    
+        } else { 
+          this.lastSeenPlayerX = angel.x;
+          this.lastSeenPlayerY = angel.y;
+          this.AISeeingAngel();
+        }
+      }
   }
   
   this.draw = function() {
